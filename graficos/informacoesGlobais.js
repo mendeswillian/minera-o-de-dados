060801ef9c38e9813859a751c3c47fff7c298421
mendeswillian <145126644+mendeswillian@users.noreply.github.com>
2024-10-07 16:21:45 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,12 +5,25 @@ async function visualizarInformacoesGlobais(){
   const res = await fetch(url)
   const dados =  await res.json()
   console.log(dados);
+  const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+  const horas = parseInt (dados.tempo_medio)
+  const minutos = Math.round((dados.tempo_medio - horas)*100)
+
+
 
 const paragrafo = document.createElement('p')
+
 paragrafo.classList.add('graficos-container_texto')
 
-paragrafo.innerHTML = `Cristiano Ronaldo 
-${dados.total_pessoas_conectadas}`
+paragrafo.innerHTML = `Você sabia que o mundo
+<span>  ${pessoasNoMundo} </span> de pessoas e aproxidamente
+<span>  ${dados.total_pessoas_conectadas} </span> estão conectadas em alguma rede social e passam em média
+<span>  ${horas} </span> : <span> ${minutos} </span> horas conectadas` 
+
+
+
+
+
 
 const container = document.getElementById("graficos-container")
 container.appendChild(paragrafo)
@@ -19,3 +32,6 @@ container.appendChild(paragrafo)
 }
 
 visualizarInformacoesGlobais();
+
+
+
